9def04cc4df99c8c01e5385a0542e1bddc3cc7cf
pgrexam <>
2020-11-15 19:29:44 +0100

    Updated statuscake test name

--- statuscake.tf
+++ statuscake.tf
@@ -3,7 +3,7 @@ provider "statuscake" {
 }
 
 resource "statuscake_test" "pgrexam" {
-  website_name = "Exam is online"
+  website_name = "Exam online"
   website_url  = google_cloud_run_service.default.status[0].url
   test_type    = "HTTP"
   check_rate   = 300
