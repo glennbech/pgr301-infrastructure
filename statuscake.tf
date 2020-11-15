3006a79ee222501ed1a7591889fb73472a86577c
pgrexam <>
2020-11-15 19:50:25 +0100

    Fixed terraform state

--- statuscake.tf
+++ statuscake.tf
@@ -3,7 +3,7 @@ provider "statuscake" {
 }
 
 resource "statuscake_test" "pgrexam" {
-  website_name = "Exam online"
+  website_name = "Exam online check"
   website_url  = format("%s/%s",google_cloud_run_service.default.status[0].url,"wishes")
   test_type    = "HTTP"
   check_rate   = 300
